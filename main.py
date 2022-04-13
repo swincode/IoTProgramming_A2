2aafb02802ede85ca43ccfd566c82a298ffae8f5
Eden <101907966@student.swin.edu.au>
2022-04-14 01:26:05 +1000

    fogot what

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import random, serial
 import asyncio
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates()
 html = """
 <!DOCTYPE html>
 <html>
@@ -43,31 +45,34 @@ html = """
 from Config import Config
 conf = Config()
 
-ser= serial.Serial(conf.PORT, conf.BAUD_RATE)
+ser= serial.Serial(conf.PORT, conf.BAUD_RATE, timeout=conf.TIMEOUT)
 
 # TODO: set serial to notify arduino of on/off and to set different effects/ colours
 
+@app.on_event("startup")
+async def startup():
+    print("Starting...")
 
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return templates.TemplateResponse(html)
 
 @app.websocket_route("/ws")
 async def websocket(websocket: WebSocket):
     await websocket.accept()
     while True:
-        rx = websocket.receive_text()
+        rx = await websocket.receive_text()
+        
         d = ser.readline()
-        if d != '':
-        #something
-            websocket.send_text(d)
-            if rx == "Turn on":
-                ser.write(bytes('1', "utf-8"))
-                send_byte = 0
-            elif rx == "Turn off":
-                ser.write(bytes('0', "utf-8"))
-                send_byte = 1
+        await websocket.send_text(f"message: {d}")
 
-@app.get("/")
-async def root():
-    return HTMLResponse(html)
+        match rx:
+            case "power":
+                ser.write(bytes('1', "utf-8"))
+            case "purple":
+                ser.write(bytes('3', "utf-8"))
+            case "blue":
+                ser.write(bytes('4', "utf-8"))
 
 def parse_serial_input() -> str:
         """
